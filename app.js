3b6e9e7dd550278c45a931f07939288ebeb9d458
Victor <naranjohoyosvictormanuel@gmail.com>
2020-05-14 23:42:58 -0500

    git commit

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     
+    //card options
+    const cardArray =[
+        {
+           name: 'fries',
+           img: 'images/fries.png' 
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'  
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        }
+    ]
 })

09c2191ce92645ab588aacecb1b6210d80491333
Victor <naranjohoyosvictormanuel@gmail.com>
2020-05-15 11:36:33 -0500

    git commit

--- app.js
+++ app.js
@@ -13,6 +13,79 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             name: 'cheeseburger',
             img: 'images/cheeseburger.png'
-        }
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/ice-milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/ice-milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
     ]
+
+const grid = document.querySelector('.grid')
+var cardsChosen =[]
+var cardsChosenId =[]
+
+//create board
+function createBoard(){
+    for(let i = 0; i < cardArray.length; i++){
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.setAttribute('click', flipcard)
+        grid.appendChild(card)
+    }
+}
+
+// check for matches
+function checkForMatch(){
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    
+} 
+
+//flip your card 
+function flipcard(){
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2){
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+createBoard()
+
 })
